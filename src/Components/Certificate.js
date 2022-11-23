5347a75f701cb9be91a710ea4ff3b451d001ac34
Rishika Vishnoi <Rishikavishnoi56@gmail.com>
2022-11-23 16:54:12 +0530

    modify

--- src/Components/Certificate.js
+++ src/Components/Certificate.js
@@ -3,8 +3,8 @@ export default function Certificate(){
     return(
         <div className="container">
        <h2 className="text-center mt-4 py-4 text-light">Certificate </h2>
-       <div className="row">
-    <img src= {require("../Images/c-1.jpg" )} alt="img" className="certi " />
+       <div className="row justify-content-center">
+     <img src= {require("../Images/c-1.jpg" )} alt="img" className="certi " />
      <img src= {require("../Images/c-3.png" )} alt="img" className="certi"/>
      <img src= {require("../Images/c-2.png" )} alt="img" className="certi" />
        </div>
